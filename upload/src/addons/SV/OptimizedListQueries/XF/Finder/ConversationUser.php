dc2e8e4282b9a262a5ecbc6ce1668586f43315a7
Xon <tazipper1@hotmail.com>
2022-01-30 17:10:54 +0800

    Avoid cases where a single row is being fetched (or all rows for a conversation)

--- upload/src/addons/SV/OptimizedListQueries/XF/Finder/ConversationUser.php
+++ upload/src/addons/SV/OptimizedListQueries/XF/Finder/ConversationUser.php
@@ -13,6 +13,10 @@ class ConversationUser extends XFCP_ConversationUser
 
     protected function getEarlyJoinThreshold(int $offset = null, int $limit = null, array $options = []): int
     {
+        if ($limit === null || $limit === 1 && $offset === 0)
+        {
+            return -1;
+        }
         return (int)(\XF::options()->sv_convlistquery_threshold ?? -1);
     }
 }
