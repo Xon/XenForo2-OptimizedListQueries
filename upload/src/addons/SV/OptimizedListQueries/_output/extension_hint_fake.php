0966a4a542903e99c3c49bbc369dca13eec6c2e9
Xon <635541+Xon@users.noreply.github.com>
2024-07-20 00:57:20 +0800

    Update extension hints file

--- upload/src/addons/SV/OptimizedListQueries/_output/extension_hint_fake.php
+++ upload/src/addons/SV/OptimizedListQueries/_output/extension_hint_fake.php
@@ -1,5 +1,11 @@
 <?php
 
+/** @noinspection PhpIllegalPsrClassPathInspection */
+
+namespace  Audentio\Feeds\Finder
+{
+    class ThreadFeedEntry extends \XF\Mvc\Entity\Finder {}
+}
 
 namespace  XF\Finder
 {
